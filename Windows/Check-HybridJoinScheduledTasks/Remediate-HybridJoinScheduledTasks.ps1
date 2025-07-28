41e6bcbc979565218b7e2751309e8bae175a8de0
Bahusafoo <huggans.sean@gmail.com>
2025-07-28 11:47:22 -0500

    25.7.28.1
    Check-HybridJoinScheduledTasks

---
+++ Windows/Check-HybridJoinScheduledTasks/Remediate-HybridJoinScheduledTasks.ps1
@@ -0,0 +1,85 @@
+﻿#########################################
+# Check-HybridJoinScheduledTasks.ps1
+$ScriptVersion = "25.7.27.1"
+# This script is intended as a ConfigMgr Compliance Item
+#########################################
+# Variables
+###################################
+$LogFile = "Check-HybridJoinScheduledTasks.log"
+$LogDir = "C:\Windows\Logs\Compliance"
+$LogPath = "$($LogDir)\$($LogFile)"
+[string[]]$CheckTasks = "Automatic-Device-Join", "Device-Sync", "Recovery-Check"
+
+#########################################
+# Functions
+###################################
+
+function Log-Action ($Message, $StampDateTime, $WriteHost)
+{
+    ################################
+    # Function Version 19.5.11.4
+    # Function by Sean Huggans
+    ################################
+	New-Item -ItemType directory -Path $LogDir -Confirm:$false -Force | out-null
+    if (($StampDateTime -eq $false) -or ($StampDateTime -eq "no")) {
+        $Message | Out-File $LogPath -Append
+    } else {
+	    "[ $(get-date -Format 'yyyy.MM.dd HH:mm:ss') v$($ScriptVersion) ] $($Message)" | Out-File $LogPath -Append
+    }
+    if ($WriteHost -eq $true) {
+        Write-Host $Message
+    }
+}
+
+function Maintain-Log {
+    Try {
+        if ($(Get-Item -Path $LogPath).Length -gt 5120) {
+            # Remove Previous Archived Log
+            $ArchivedLogs = Get-ChildItem -Path $LogDir | Where-Object {(($_.Name -like "ARCHIVED-*") -and ($_.Extension -eq ".log"))}
+            foreach ($ArchivedLog in $ArchivedLogs) {
+                Remove-Item -Path $ArchivedLog.FullName -Force
+                Log-Action -Message "Log Maintenance: Removed $($ArchivedLog.FullName)"
+            }
+            Rename-Item -Path $LogPath -NewName "ARCHIVED-$($LogFile.replace('.log',''))-$(Get-date -Format 'yyyyMMdd-HHmmss').log" -Force
+            Log-Action -Message "The previous log was over 5120 in size and has been archived."
+        }
+    } catch {
+        Log-Action "Error: Maintain Log Failed ($($PSItem.ToString()))."
+    }
+}
+
+#########################################
+# Execution Logic
+###################################
+Maintain-Log
+
+Log-Action -Message "Starting Remediation..."
+[bool]$AllChecked = $True
+[bool]$AllEnabled = $True
+foreach ($CheckTask in $CheckTasks) {
+    Remove-Variable -Name "TaskObject" -Force -ErrorAction SilentlyContinue
+    Try {
+        $TaskObject = Get-ScheduledTask -TaskName $CheckTask -ErrorAction Stop
+        if ($TaskObject.State -eq "Disabled") {
+            Try {
+                Enable-ScheduledTask -TaskName $CheckTask -TaskPath "Microsoft\Windows\Workplace Join" -ErrorAction Stop | Out-Null
+                Log-Action -Message "Scheduled Task has been Enabled ($($CheckTask))."
+            } catch {
+                Log-Action -Message "Error: Failed to enable Scheduled Task ($($CheckTask) - ($($psItem.ToString())))."
+                $AllEnabled = $False
+            }
+        } else {
+            Log-Action -Message "Scheduled Task state was not Disabled ($($CheckTask))."
+        }
+    } catch {
+        Log-Action -Message "Error: Scheduled Task Not Found ($($CheckTask))."
+        $AllChecked = $False
+    }
+}
+
+if (($AllChecked -eq $True) -and ($AllEnabled -eq $True)) {
+    Log-Action -Message "Remediation Result: Success"
+} else {
+    Log-Action -Message "Remediation Result: Fail"
+}
+    Log-Action -Message "Finished Remediation."
